775d1be9a3691b9f940f7a8e0b8150dc6a28a5ab
Sander <99058223@mydavinci.nl>
2019-10-30 11:01:00 +0100

    test

--- cafe.js
+++ cafe.js
@@ -1,19 +1,27 @@
 function drank(){
+	for (a=1; a<=3; a++){
 	 var drank = prompt('Wat wilt u drinken? Kies uit: bier, fris of wijn?');
 	 if (drank == 'bier'){
 	 	var bier = prompt('Hoeveel bier wilt u toevoegen aan uw bestelling?')
 	 	var nog = prompt('Wilt u nog een ander drankje toevoegen aan uw bestelling? ja of nee?')
+	 	if (nog == 'nee'){
+	 		return false;
+	 	}
 	 }
 	 else if (drank == 'fris'){
 	 	var fris = prompt('Hoeveel fris wilt u toevoegen aan uw bestelling?')
 	 	var nog = prompt('Wilt u nog een ander drankje toevoegen aan uw bestelling? ja of nee?')
+	 	if (nog == 'nee'){
+	 		return false;
+	 	}
 	 }
 	 else if (drank == 'wijn'){
 	 	var wijn = prompt('Hoeveel wijn wilt u toevoegen aan uw bestelling?')
 	 	var nog = prompt('Wilt u nog een ander drankje toevoegen aan uw bestelling? ja of nee?')
+	 	if (nog == 'nee'){
+	 		return false;
+	 	}
 	 }
-	 if (nog = 'ja'){
-	 	drank();
-	 }
-}
+	}//loop
+}//function
 drank();
